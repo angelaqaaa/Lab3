d85eeef1fc6f90b9a4f36da8756d43e02527220d
Angela Su <angelasumas@gmail.com>
2025-06-02 09:50:25 -0400

    Lab Completed

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-// TODO CheckStyle: Wrong lexicographical order for 'java.util.HashMap' import (remove this comment once resolved)
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,11 +13,13 @@ import java.util.Map;
  */
 public class CountryCodeConverter {
 
-    // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
+    private static final int MIN_COLUMNS = 3;
+    private final Map<String, String> codeToName;
+    private final Map<String, String> nameToCode;
 
     /**
      * Default constructor which will load the country codes from "country-codes.txt"
-     * in the resources folder.
+     * in the resources' folder.
      */
     public CountryCodeConverter() {
         this("country-codes.txt");
@@ -30,13 +31,24 @@ public class CountryCodeConverter {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public CountryCodeConverter(String filename) {
+        codeToName = new HashMap<>();
+        nameToCode = new HashMap<>();
 
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
 
-            // TODO Task: use lines to populate the instance variable(s)
+            for (int i = 1; i < lines.size(); i++) {
+                String line = lines.get(i);
+                String[] parts = line.split("\t");
+                if (parts.length >= MIN_COLUMNS) {
+                    String countryName = parts[0].trim();
+                    String alpha3Code = parts[2].trim();
 
+                    codeToName.put(alpha3Code.toLowerCase(), countryName);
+                    nameToCode.put(countryName, alpha3Code.toLowerCase());
+                }
+            }
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -50,8 +62,7 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return code;
+        return codeToName.getOrDefault(code.toLowerCase(), code);
     }
 
     /**
@@ -60,8 +71,7 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return country;
+        return nameToCode.getOrDefault(country, country);
     }
 
     /**
@@ -69,7 +79,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this code converter.
      */
     public int getNumCountries() {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return 0;
+        return codeToName.size();
     }
 }
--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -5,9 +5,12 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
@@ -15,7 +18,8 @@ import org.json.JSONArray;
  */
 public class JSONTranslator implements Translator {
 
-    // TODO Task: pick appropriate instance variables for this class
+    private final Map<String, JSONObject> countryData;
+    private final List<String> countries;
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -30,6 +34,9 @@ public class JSONTranslator implements Translator {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public JSONTranslator(String filename) {
+        countryData = new HashMap<>();
+        countries = new ArrayList<>();
+
         // read the file to get the data to populate things...
         try {
 
@@ -37,8 +44,12 @@ public class JSONTranslator implements Translator {
 
             JSONArray jsonArray = new JSONArray(jsonString);
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject country = jsonArray.getJSONObject(i);
+                String countryCode = country.getString("alpha3");
+                countryData.put(countryCode, country);
+                countries.add(countryCode);
+            }
 
         }
         catch (IOException | URISyntaxException ex) {
@@ -48,21 +59,32 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        JSONObject countryObj = countryData.get(country);
+        if (countryObj == null) {
+            return new ArrayList<>();
+        }
+
+        List<String> languages = new ArrayList<>();
+        for (String key : countryObj.keySet()) {
+            // Skip non-language fields
+            if (!"id".equals(key) && !"alpha2".equals(key) && !"alpha3".equals(key)) {
+                languages.add(key);
+            }
+        }
+        return new ArrayList<>(languages);
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return new ArrayList<>(countries);
     }
 
     @Override
     public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
-        return null;
+        JSONObject countryObj = countryData.get(country);
+        if (countryObj == null || !countryObj.has(language)) {
+            return null;
+        }
+        return countryObj.getString(language);
     }
 }
